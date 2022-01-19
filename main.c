028b0c83cf0f1afc78ff1bbe419ffdb1f7eaffae
nik5140 <75437508+nik5140@users.noreply.github.com>
2022-01-19 23:39:48 +0700

    some functions added
    A9, A10, M1 functions added. If compiling this project in linux - you should add -lm key in compiler settings (compiler flags).

--- main.c
+++ main.c
@@ -139,11 +139,70 @@ int productAbsArr(int*x, int n)
 
 float SqOfSumOfPos(int*x, int n)
 {
-    return sqrt(summPosArr(x, n));
+    float result=0;
+    int summ = 0;
+    summ = summPosArr(x,n);
+    result = sqrt((float)summ);
+    return result;
 }
+
+//A9 natural log of summ of abs of elements in array
+float NatLogOfSummofAbsArr(int*x, int n)
+{
+    int i = 0;
+    int logSummArr = 1;
+
+    for (i=0; i<n; i++)
+    {
+        if (x[i]>=0)
+            logSummArr+=x[i];
+        else if (x[i]<0)
+            logSummArr+=-x[i];
+    }
+
+
+    return logf(logSummArr);
+}
+
+// A10 Sum of sqrts of positive elements of array
+float summSqrPosArr(int*x, int n)
+{
+    int i;
+    float summ=0;
+    for (i=0; i<n; i++)
+    {
+        if (x[i]>0)
+            summ+=sqrt(x[i]);
+    }
+
+    return summ;
+}
+
+//M1 N of elements in array that are lower than A1
+int NElLowerThenA1(int*x,int n)
+{
+    int N=0;
+    int A1=0;
+    A1 = summArr(x, n);
+    printf("A1 is %d", A1);
+    for (int i=0; i<n; i++)
+    {
+        if (x[i]<A1)
+           {
+           printf("%d\n", x[i]);
+           N++;
+
+           }
+
+    }
+    return N;
+
+}
+
 
 int main()
 {
+    system("-lm");
     srand(time(NULL));
     int * mass;
     int n;
@@ -156,12 +215,15 @@ int main()
     printf("\nSumm of all elements in array is: %d!\n", summArr(mass, n));
     printf("\nSumm of all positive elements in array is: %d!\n", summPosArr(mass, n));
     printf("\nSumm of all negative elements in array is: %d!\n", summNegArr(mass, n));
-    printf("\N of all zero elements in array is: %d!\n", NZeroEl(mass, n));
-    printf("\Min is %d and Max is %d \n", MinArr(mass, n), MaxArr(mass, n));
-    printf("\Sum of Min and Max is %d \n", sumMinMax(mass, n));
-    printf("\Sredn ariphm of elements in arr is %3.2f \n", SrednAriphm(mass, n));
-    printf("\Product of abs of elements in array is %3d \n", productAbsArr(mass, n));
-    printf("\Sqrt of summ of positive elements in array is %3.2f \n", SqOfSumOfPos(mass, n));
+    printf("\n N of all zero elements in array is: %d!\n", NZeroEl(mass, n));
+    printf("\n Min is %d and Max is %d \n", MinArr(mass, n), MaxArr(mass, n));
+    printf("\n Sum of Min and Max is %d \n", sumMinMax(mass, n));
+    printf("\n Sredn ariphm of elements in arr is %3.2f \n", SrednAriphm(mass, n));
+    printf("\n Product of abs of elements in array is %3d \n", productAbsArr(mass, n));
+    printf("\n Sqrt of summ of positive elements in array is %3.2f \n", SqOfSumOfPos(mass, n));
+    printf("\n NatLog of summ of abs of elements in array is %3.2f \n", NatLogOfSummofAbsArr(mass, n));
+    printf("\n Summ of sqrts of positive elements in array is %3.2f \n", summSqrPosArr(mass, n));
+    printf("\n N of elements in X that are lower then A1 is %d\n", NElLowerThenA1(mass, n));
     return 0;
 }
 
